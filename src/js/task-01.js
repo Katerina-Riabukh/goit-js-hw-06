552465f7ec8e6cfb7b909a06473f5b3372d7c335
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-04-25 22:02:12 +0200

    22.02

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+
+//const list = document.getElementById('categories');
+// const list = document.querySelector('#categories')
+// console.log(list);
+
+// const caunter = 0;
+
+// list.forEach(function (li) {
+//     console.log(`Number of categories: ${caunter += li}`);
+//     //return `Number of categories: ${caunter += li}`;
+// });
+
+
+console.log(document);
