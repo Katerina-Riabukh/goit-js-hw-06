4e09789f6123dffe8b233d1489e75f881f913b0c
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-04-28 15:01:00 +0200

    task-2, task-3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,34 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const list = document.querySelector('.gallery');
+//console.log(list);
+
+const img = images.map(({ url, alt }) => {
+  const itemOfList = `<li class="list-item"><img src="${url}" alt="${alt}"></img></li>`;
+  return itemOfList
+}).join('');
+
+//console.log(img);
+
+list.insertAdjacentHTML("afterbegin", img);
+
+//list.classList.add('improve');
+//list.style.display = 'grid'
+
+const item = document.querySelectorAll('.list-item')
+//console.log(item);
+
+const imageEl = document.querySelectorAll('img')
+//console.log(imageEl);
+
+const size = imageEl.forEach(function (image) {
+  image.width = 320;
+  image.height = 190;
+});
+
+
+
+
