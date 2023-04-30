ab0779648be732164005cc98bb9d14ceef46b49a
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-04-30 18:28:07 +0200

    task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,17 +6,15 @@ const newElements = [...elements];
 console.log(`Number of categories: ${elements.length}`);
 
 const getQuantityOfElem = newElements.forEach(function (element, index, array) {
-    //console.log(element);
+   
     const firstChildEl = element.firstElementChild;
-    //console.log(firstChildEl);
+   
     const name = firstChildEl.textContent;
-    //console.log(name);
+    
      const lastChildEl = element.lastElementChild;
-    //console.log(lastChildEl);
+   
     const arreyOfChildren = lastChildEl.children;
-    //console.log(arreyOfChildren);
-    //console.log(arreyOfChildren.length);
-    //console.log(`Category: ${name}, Elements: ${arreyOfChildren.length}`);
+
     console.log(`Category: ${name}`);
     console.log(`Elements: ${arreyOfChildren.length}`);
 });
